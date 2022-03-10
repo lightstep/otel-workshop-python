88a01ae8f70aa1fa5a19141473682a8ecd312d31
AjohnsonLS <62617632+AjohnsonLS@users.noreply.github.com>
2022-03-10 13:21:26 -0800

    Initial cleaning
    Clean initial set up to establish clean baseline

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# otel-workshop-python
-Workshop to work through Python manual instrumentation examples
+# Overview
+
+This example shows how to use OpenTelemetry's API to create, augment, and report spans. The goal is for us to be able to create spans and ultimately see them in the Lightstep UI. 
+
+## Setup
+
+* * *
+
+* You will need Python 3 installed
+* You will need pip3 installed
+* You will need to run:
+```
+	pip3 install opentelemetry-api
+	pip3 install opentelemetry-sdk
+	pip3 install opentelemetry-exporter-otlp
+```
+
+## Run the Application
+
+* * *
+
+1. Run the program:
+```
+	python bubbleSort.py
+```
+
+You should see traces exported to your console and your instance of Lightstep if you appropriately configured your access token.
+
+## Useful Links
+
+* * *
+* OpenTelemetry: https://github.com/open-telemetry/opentelemetry-python/
+
+## License
+
+* * *
+Apache License 2.0
--- bubbleSort.py
+++ bubbleSort.py
@@ -1,25 +1,29 @@
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
     ConsoleSpanExporter,
 )
 
+#Define resource to identify our service
+resource = Resource.create({"service.name": "basic_service"})
 
-trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(ConsoleSpanExporter())
-)
+#configure initial tracer
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
 tracer = trace.get_tracer(__name__)
-with tracer.start_as_current_span("foo"):
-    print("Hello world!")
-
 
 def bubble_sort(data):
-    for _ in range(len(data)):
-        for i in range(len(data) - 1):
-            if data[i] > data[i + 1]:
-                data[i], data[i + 1] = data[i + 1], data[i]
+    with tracer.start_as_current_span("root"):
+        for _ in range(len(data)):
+            for i in range(len(data) - 1):
+                if data[i] > data[i + 1]:
+                    data[i], data[i + 1] = data[i + 1], data[i]
     return data
 
 a_list = [2, 1, 5, 4, 3]
