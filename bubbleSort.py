ac94150d660e09da5059b92e7e3b01e395e1ce23
AjohnsonLS <62617632+AjohnsonLS@users.noreply.github.com>
2022-03-10 16:50:34 -0800

    Full instrumentation
    Added attributes, error handling, and events

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,5 +1,6 @@
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.trace.status import Status, StatusCode
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace.export import (
@@ -21,27 +22,48 @@ tracer = trace.get_tracer(__name__)
 
 def bubbleSort(arr):
     n = len(arr)
- 
     # Traverse through all array elements
     for i in range(n-1):
     # range(n) also work but outer loop will
     # repeat one time more than needed.
- 
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
- 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j + 1] :
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
- 
+        with tracer.start_as_current_span("outerLoop"):   
+            current_span = trace.get_current_span()
+            current_span.set_attribute("outer iteration", i)
+            if i > 10 :
+                current_span.set_status(Status(StatusCode.ERROR))
+                raise Exception("Array too large! Stop execution")
+                break
+                # Last i elements are already in place
+            for j in range(0, n-i-1):
+                with tracer.start_as_current_span("innerLoop"):
+                    current_span = trace.get_current_span()
+                    current_span.set_attribute("inner iteration", j)
+                # traverse the array from 0 to n-i-1
+                # Swap if the element found is greater
+                # than the next element
+                    if arr[j] > arr[j + 1] :
+                        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                        current_span.add_event("swapLog", {
+                            "inner iteration": j,
+                            "log.severity": "event",
+                            "log.message": "Swap occurred"
+                            })
+     
 # Driver code to test above
-arr = [64, 34, 25, 12, 22, 11, 90]
+arr = [64, 34, 25, 12, 22, 11, 90, 100, 1231, 4213, 432, 33, 42, 62]
  
-with tracer.start_as_current_span("root"):
-    bubbleSort(arr)
- 
+with tracer.start_as_current_span("parent"):
+    current_span = trace.get_current_span()
+    current_span.set_attribute("machineName", "InstructorMachine")
+    current_span.set_attribute("owner", "#PythonInstrumentation")
+    current_span.set_attribute("number", "4")
+    try:
+        bubbleSort(arr)
+    except Exception as ex:
+        current_span.record_exception(ex)
+        current_span.set_status(Status(StatusCode.ERROR, ex))
+
+
 print ("Sorted array is:")
 for i in range(len(arr)):
     print ("% d" % arr[i],end=" ")
